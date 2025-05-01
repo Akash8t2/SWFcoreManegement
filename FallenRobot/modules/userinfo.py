a58cccd7a656f9f7951196372f9c7a52b9c9ffbf
Akash <158745842+Akash8t2@users.noreply.github.com>
2025-05-01 22:37:59 +0530

    Update userinfo.py

--- FallenRobot/modules/userinfo.py
+++ FallenRobot/modules/userinfo.py
@@ -324,10 +324,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url="https://t.me/FallenAssociation/7"
+                                "ʜᴇᴀʟᴛʜ", url="https://t.me/SFWBots_Official"
                             ),
                             InlineKeyboardButton(
-                                "ᴅɪꜱᴀꜱᴛᴇʀ", url="https://t.me/FallenAssociation/8"
+                                "ᴅɪꜱᴀꜱᴛᴇʀ", url="https://t.me/SFWBots_Official"
                             ),
                         ],
                     ]
