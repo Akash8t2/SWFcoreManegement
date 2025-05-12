06c359333cd36c7fd21170feab394fc86cf79316
Akash <158745842+Akash8t2@users.noreply.github.com>
2025-05-12 10:47:58 +0530

    Update chatbot_sql.py

--- FallenRobot/modules/sql/chatbot_sql.py
+++ FallenRobot/modules/sql/chatbot_sql.py
@@ -1,9 +1,6 @@
 import threading
-
 from sqlalchemy import Column, String
-
 from FallenRobot.modules.sql import BASE, SESSION
-
 
 class FallenChats(BASE):
     __tablename__ = "fallen_chats"
@@ -12,31 +9,24 @@ class FallenChats(BASE):
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
-
 FallenChats.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
-
 def is_fallen(chat_id):
-    try:
-        chat = SESSION.query(FallenChats).get(str(chat_id))
-        return bool(chat)
-    finally:
-        SESSION.close()
-
+    chat = SESSION.query(FallenChats).get(str(chat_id))
+    return bool(chat)
 
 def set_fallen(chat_id):
     with INSERTION_LOCK:
         fallenchat = SESSION.query(FallenChats).get(str(chat_id))
         if not fallenchat:
             fallenchat = FallenChats(str(chat_id))
-        SESSION.add(fallenchat)
+            SESSION.add(fallenchat)
         SESSION.commit()
-
 
 def rem_fallen(chat_id):
     with INSERTION_LOCK:
         fallenchat = SESSION.query(FallenChats).get(str(chat_id))
         if fallenchat:
             SESSION.delete(fallenchat)
-        SESSION.commit()
+            SESSION.commit()
